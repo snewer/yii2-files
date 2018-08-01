f8d7c26a5ab6d0ff4d9ad04e4a8650b830d7b5e5
Kirill Kasyanov <zions@bk.ru>
2018-08-01 19:26:54 +0400

    small fix

--- src/Module.php
+++ src/Module.php
@@ -1,7 +1,66 @@
 <?php
+
+namespace snewer\files;
+
+use Yii;
+use yii\filters\AccessControl;
+use yii\base\InvalidConfigException;
+
 /**
- * Created by PhpStorm.
- * User: lenovo
- * Date: 01.08.2018
- * Time: 18:38
+ * Class Module
+ * @package snewer\images
+ * @property \snewer\storage\StorageManager $storage
  */
+class Module extends \yii\base\Module
+{
+
+    /**
+     * @var string|\snewer\storage\StorageManager
+     */
+    private $_storage = 'storage';
+
+    public $bucketName;
+
+    public $controllerAccess;
+
+    public function init()
+    {
+        parent::init();
+        if ($this->bucketName === null) {
+            throw new InvalidConfigException('Необходимо установить название хранилища для загрузки файлов \'Module::$bucketName\'.');
+        }
+        if ($this->controllerAccess === null) {
+            $this->controllerAccess = [
+                'class' => AccessControl::className(),
+                'rules' => [
+                    [
+                        'allow' => true,
+                        'roles' => ['@']
+                    ]
+                ]
+            ];
+        }
+    }
+
+    public function setStorage($value)
+    {
+        if (is_string($value)) {
+            $this->_storage = $value;
+        } else {
+            $this->_storage = Yii::createObject($value);
+        }
+    }
+
+    /**
+     * @return \snewer\storage\StorageManager
+     */
+    public function getStorage()
+    {
+        if (is_string($this->_storage)) {
+            return Yii::$app->get($this->_storage, true);
+        } else {
+            return $this->_storage;
+        }
+    }
+
+}
