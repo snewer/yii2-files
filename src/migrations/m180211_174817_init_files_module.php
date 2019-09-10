8543cc7a64f7850288ab58b7147a87cbaa2b3cab
Kirill Kasyanov <snewer0@gmail.com>
2019-09-10 15:42:41 +0400

    Правка кодировки таблицы

--- src/migrations/m180211_174817_init_files_module.php
+++ src/migrations/m180211_174817_init_files_module.php
@@ -11,6 +11,11 @@ class m180211_174817_init_files_module extends Migration
      */
     public function safeUp()
     {
+        $tableOptions = null;
+        if ($this->db->driverName === 'mysql') {
+            $tableOptions = 'CHARACTER SET utf8 COLLATE utf8_general_ci ENGINE=InnoDB';
+        }
+
         $this->createTable('{{%files}}', [
             'id' => $this->primaryKey()->unsigned(),
             'bucket_id' => $this->integer()->unsigned(),
@@ -20,11 +25,11 @@ class m180211_174817_init_files_module extends Migration
             'integrity' => $this->string(),
             'uploaded_at' => $this->integer()->unsigned(),
             'uploaded_by' => $this->integer()->unsigned()
-        ]);
+        ], $tableOptions);
         $this->createTable('{{%files_buckets}}', [
             'id' => $this->primaryKey()->unsigned(),
             'name' => $this->string()
-        ]);
+        ], $tableOptions);
         $this->createIndex('un_files_buckets$name', '{{%files_buckets}}', 'name', true);
     }
 
